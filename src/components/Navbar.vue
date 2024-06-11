dc7b585f6543f1a20fd7967187f557fb39d7fdf7
Daniel <danielfealmeida@gmail.com>
2024-06-11 18:12:11 -0300

    create navbar and sidenav components

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="navx fixed left-0 top-0 w-screen border-b-2 border-stone-200 flex flex-row">
+        <div class="w-10 h-10 bg-blue-500 my-auto mr-auto ml-20"></div>
+        <div class="h-10 my-auto mx-auto text-center align-middle inline-block content-center"><p>Página inicial</p></div>
+        <div class="h-10 my-auto mx-auto text-center align-middle inline-block content-center"><p>Sobre</p></div>
+        <div class="h-10  my-auto mx-auto text-center align-middle inline-block content-center"><p>Tutoriais</p></div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="css">
+    .navx {
+        height: 8vh;
+    }
+</style>
---
+++ src/components/SideNav.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="nav fixed left-0 bottom-0 w-40 border-r-2 border-stone-200 flex flex-col pt-5">
+        <div class="w-10/12 h-10 my-5 mx-auto text-center align-middle inline-block content-center hover:rounded-lg hover:bg-highlight-gray"><p>Anotações</p></div>
+        <div class="w-10/12 h-10 my-5 mx-auto text-center align-middle inline-block content-center hover:rounded-lg hover:bg-highlight-gray"><p>Fórum</p></div>
+        <div class="w-10/12 h-10 my-5 mx-auto text-center align-middle inline-block content-center hover:rounded-lg hover:bg-highlight-gray"><p>Questões</p></div>
+        <div class="w-10/12 h-10 my-5 mx-auto text-center align-middle inline-block content-center hover:rounded-lg hover:bg-highlight-gray"><p>Perfil</p></div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="css">
+    .nav {
+        height: 92vh;
+    }
+</style>
