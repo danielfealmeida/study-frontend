e434dd4a1f02080c248224b1110523a568e3b0e7
Daniel <danielfealmeida@gmail.com>
2024-06-11 18:14:14 -0300

    add navbar and sidebar to app

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,13 @@ import { RouterView } from "vue-router";
 import { collection, addDoc } from "firebase/firestore";
 import firebaseInit from "./firebaseInit";
 
+import Navbar from "./components/Navbar.vue"
+
 // addDoc(collection(firebaseInit.firestore, "test"), {id: 1, name: "002", isReal:true})
 </script>
 
 <template>
+  <Navbar/>
   <RouterView/>
 </template>
 
--- src/views/StudyView.vue
+++ src/views/StudyView.vue
@@ -1,7 +1,24 @@
+<script>
+  import SideNav from "../components/SideNav.vue"
+  export default {
+  components: {
+    SideNav
+  }
+}
+</script>
+
 <template>
-  <h1 class="text-red-600">This is an about page</h1>
+  <SideNav/>
+  <div class="body">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in maximus libero. Nulla facilisi. Praesent pulvinar dapibus nisl id placerat. Vestibulum id feugiat est, a ultricies ligula. Mauris eget sollicitudin nunc. Praesent eget justo id orci congue scelerisque quis vel tellus. Aenean ornare pellentesque eleifend. Curabitur sapien lectus, tincidunt in maximus nec, sodales non ante. Etiam sollicitudin dolor quis varius pharetra. Pellentesque faucibus pellentesque massa, rhoncus rhoncus nunc porta sed. Sed scelerisque gravida augue vel pretium. Cras viverra accumsan eleifend. Fusce viverra est at lobortis auctor. Suspendisse tincidunt pellentesque eros sed rutrum. Suspendisse vel purus eget urna feugiat pulvinar vitae nec sem.
+    Nullam cursus fringilla orci vel accumsan. Praesent in ante tempor, dapibus orci ut, dictum orci. Phasellus arcu nulla, ultricies non nisl vitae, mattis tempor tortor. Morbi id nibh nunc. Curabitur lacinia convallis leo, pellentesque hendrerit lacus. Sed hendrerit mattis nunc sed pulvinar. Morbi tincidunt diam ultricies velit dapibus condimentum. Maecenas neque lectus, aliquam a tincidunt a, ultricies ut arcu. Nulla euismod finibus sollicitudin. Vivamus at vehicula mauris. Praesent eu semper libero. Pellentesque tristique nisl elit, non maximus nisi pulvinar sit amet.
+  </div>
 </template>
 
 <style>
-
+  .body {
+    padding-top: 9rem;
+    padding-left: 16rem;
+    padding-right: 6rem;
+  }
 </style>
