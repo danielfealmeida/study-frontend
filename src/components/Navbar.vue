f0596a2cb9bac2ffc1420fff50d6eaa6d3f83cf8
Daniel <danielfealmeida@gmail.com>
2024-06-18 14:19:25 -0300

    add links between pages

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,28 +1,28 @@
 <script>
-import {useDark, useToggle} from "@vueuse/core"
+    import {useDark, useToggle} from "@vueuse/core"
 
-let isDark = useDark();
-const toggleDark = useToggle(isDark);
+    let isDark = useDark();
+    const toggleDark = useToggle(isDark);
 
-export default {
-    data () {
-        return {
-            isDark
-        }
-    },
-    methods: {
-        changeMode: function() {
-            toggleDark();
+    export default {
+        data () {
+            return {
+                isDark
+            }
+        },
+        methods: {
+            changeMode: function() {
+                toggleDark();
+            }
         }
     }
-}
 </script>
 
 <template>
     <div class="navx fixed left-0 top-0 w-screen border-b-2 border-stone-200 flex flex-row dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line">
         <div class="w-10 h-10 bg-blue-500 my-auto mr-auto ml-20"></div>
-        <div class="h-10 my-auto mx-auto text-center align-middle inline-block content-center"><p>Página inicial</p></div>
-        <div class="h-10 my-auto mx-auto text-center align-middle inline-block content-center"><p>Sobre</p></div>
+        <div class="h-10 my-auto mx-auto text-center align-middle inline-block content-center"><a href="/">Página inicial</a></div>
+        <div class="h-10 my-auto mx-auto text-center align-middle inline-block content-center"><a href="/app">Aplicativo</a></div>
         <div class="h-10 my-auto mx-auto text-center align-middle inline-block content-center"><button @click="changeMode()">{{ isDark ? "🌙" : "☀️" }}</button></div>
     </div>
 </template>
