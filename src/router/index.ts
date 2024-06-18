3f5b22cd39f1131ffefc042338b68de5c492893f
Daniel <danielfealmeida@gmail.com>
2024-06-18 15:38:06 -0300

    add paths to signin and login pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LandingView from '../views/LandingView.vue'
 import StudyView from '../views/StudyView.vue'
+import LoginView from '../views/LoginView.vue'
+import SigninView from '../views/SigninView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +16,16 @@ const router = createRouter({
       path: '/app',
       name: 'study',
       component: StudyView
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginView
+    },
+    {
+      path: "/signin",
+      name: "signin",
+      component: SigninView
     }
   ]
 })
