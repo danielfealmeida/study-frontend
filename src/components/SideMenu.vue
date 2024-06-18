e3306065d4bc1a5fba2612853ed22edfff42d947
Daniel <danielfealmeida@gmail.com>
2024-06-18 15:38:19 -0300

    fix styles

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -10,7 +10,7 @@
 
 <template>
     <div class="side fixed right-0 w-60 h-full border-b-2 border-l-2 border-stone-200 dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-l-1 dark:border-dark-line align-middle text-center pt-5">
-        <select class="w-10/12 dark:bg-stone-800 dark:text-white" v-model="opened" @click="console.log(opened)">
+        <select class="w-10/12 bg-white dark:bg-stone-800 dark:text-white" v-model="opened" @click="console.log(opened)">
             <option value="tasks">Tarefas</option>
             <option value="pomodoro">Pomodoro</option>
         </select>
