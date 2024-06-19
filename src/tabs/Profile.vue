e4901d885cbdc9c68a43b4ea6a22317f4c7a4881
Daniel <danielfealmeida@gmail.com>
2024-06-19 19:01:10 -0300

    create ui on profile page

--- src/tabs/Profile.vue
+++ src/tabs/Profile.vue
@@ -1,12 +1,35 @@
 <script>
+    import { auth } from "../firebaseInit.ts"
+    import router from "../router/index.ts"
+    import {store} from "../store/store.ts"
+
+    let user;
+
     export default {
-        
+        data () {
+            return {
+                user
+            }
+        },
+        beforeCreate () {
+            user = auth.currentUser
+        },
+        methods: {
+            signOutUser: () => {
+                auth.signOut(user)
+                store.isLogged = false
+                router.push("/")
+            }
+        }
     }
 </script>
 
 <template>
     <div>
-        Perfil
+        <h1 class="text-lg font-bold mb-10">Perfil</h1>
+        <p class="mb-5">Email: {{ user.email }}</p>
+        <p class="mb-10">UID: {{ user.uid }}</p>
+        <button @click="signOutUser()" class="w-2/5 h-8 mx-auto mb-auto text-center border-2 border-stone-200 rounded-full dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line">Sair</button>
     </div>
 </template>
 
