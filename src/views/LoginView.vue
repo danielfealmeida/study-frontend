f2288b5744f17b488999fafb588f5bf03e696ba6
Daniel <danielfealmeida@gmail.com>
2024-06-22 19:18:58 -0300

    add enter submit to login and signin

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -32,9 +32,9 @@
     <div class="login fixed top-1/2 left-1/2 border-2 border-stone-200 rounded-sm dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line flex flex-col">
         <h1 class="mt-10 mb-20 text-center font-bold text-lg">Entrar</h1>
         <label class="w-4/5 mx-auto">E-mail:</label>
-        <input type="email" v-model="email" class="w-4/5 h-8 mx-auto mt-1 text-center border-2 border-stone-200 rounded-full dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line"/>
+        <input type="email" v-model="email" class="w-4/5 h-8 mx-auto mt-1 text-center border-2 border-stone-200 rounded-full dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line" @keypress.enter="sendData(email, password)"/>
         <label class="w-4/5 mx-auto mt-10">Senha:</label>
-        <input type="password" v-model="password" class="w-4/5 h-8 mx-auto mt-1 text-center border-2 border-stone-200 rounded-full dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line"/>
+        <input type="password" v-model="password" class="w-4/5 h-8 mx-auto mt-1 text-center border-2 border-stone-200 rounded-full dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line" @keypress.enter="sendData(email, password)"/>
         <p class="text-red-500 text-center text-sm mt-10">{{ errorCode }}</p>
         <button @click="sendData(email, password)" class="w-2/5 h-8 mx-auto mt-10 mb-auto text-center border-2 border-stone-200 rounded-full dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line">Entrar</button>
         <a href="/signin" class="w-2/5 h-8 mx-auto mb-auto text-center dark:bg-stone-800 dark:text-white text-sm">Criar conta</a>
