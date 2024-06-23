05ada7e62e076834e991bd294bce293927c0f730
Daniel <danielfealmeida@gmail.com>
2024-06-23 16:54:29 -0300

    add create and read functions to to-do

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,14 +1,26 @@
 <script>
+    import { auth, db } from "../firebaseInit"
+    import { onAuthStateChanged } from "firebase/auth"
+    import { addDoc, collection, query, where, getDocs } from "firebase/firestore"
+
     let tasks = []
     let uncompletedTasks = []
     let completedTasks = []
 
-    tasks.forEach((task) => {
-        if(task.completion == true) {
-            completedTasks.push(task)
-        } else {
-            uncompletedTasks.push(task)
-        }
+    // Reffer to comment on router code
+    const getCurrentUser = () => {
+        return new Promise((resolve, reject) => {
+            const removeListerner = onAuthStateChanged(auth, (user) => {
+            removeListerner();
+            resolve(user)
+            }, reject)
+        })
+    }
+
+    // Gets all tasks that the user has ever made and then goes through them, saving all of their data to the tasks array
+    let queryTasks = await getDocs(query(collection(db, "tasks"), where("user", "==", (await getCurrentUser()).uid)))
+    queryTasks.forEach((task) => {
+        tasks.push(task.data())
     })
 
     export default {
@@ -16,37 +28,39 @@
             return {
                 opened: "tasks",
                 uncompletedTasks,
-                completedTasks
+                completedTasks,
+                newTask: "",
             }
         },
+        created() {
+            this.cycleTasks()
+        },
         methods: {
+            // This function deletes the current uncompleted and completed tasks, then adds them back from a more recent tasks array
+            cycleTasks: function() {
+                this.uncompletedTasks = []
+                this.completedTasks = []
+                tasks.forEach((task) => {
+                    if(task.completion == true) {
+                        this.completedTasks.push(task)
+                    } else {
+                        this.uncompletedTasks.push(task)
+                    }
+                })
+            },
             changeCompletion: function(task) {
                 task.completion = !task.completion
-                this.uncompletedTasks = []
-                this.completedTasks = []
-                tasks.forEach((task) => {
-                    if(task.completion == true) {
-                        this.completedTasks.push(task)
-                    } else {
-                        this.uncompletedTasks.push(task)
-                    }
-                })
+                this.cycleTasks();
             },
             addTask: function(content) {
-                tasks.push({content, completion:false})
-                this.uncompletedTasks.push({content, completion:false})
+                let addedTask = {content, completion:false, id:tasks.length, user: auth.currentUser.uid}
+                tasks.push(addedTask)
+                this.uncompletedTasks.push(addedTask)
+                addDoc(collection(db, "tasks"), addedTask)
             },
             removeTask: function(task) {
-                tasks.splice(tasks.indexOf(task))
-                this.uncompletedTasks = []
-                this.completedTasks = []
-                tasks.forEach((task) => {
-                    if(task.completion == true) {
-                        this.completedTasks.push(task)
-                    } else {
-                        this.uncompletedTasks.push(task)
-                    }
-                })
+                tasks = tasks.filter(e => e.id !== task.id)
+                this.cycleTasks();
             }
         }
     }
@@ -60,10 +74,9 @@
         </select>
         <div v-if="opened=='tasks'" class="mt-5">
             <input type="text" v-model="newTask" @keypress.enter="addTask(newTask)" class="w-4/5 h-8 mx-auto mt-1 text-center text-sm border-2 border-stone-200 rounded-full dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line"/>
-            <button @click="addTask(newTask)" class="w-4/5 h-8 mx-auto mt-1 mb-3 text-center border-2 border-stone-200 rounded-full dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line text-sm">Adicionar</button>
+            <button @click="addTask(newTask)" class="w-4/5 h-8 mx-auto mt-1 mb-4 text-center border-2 border-stone-200 rounded-full dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line text-sm">Adicionar</button>
             <div class="flex flex-col">
                 <div class="uncompleted">
-                    <h2 class="text-left text-sm w-10/12 mx-auto mb-2">Tarefas:</h2>
                     <div v-for="task in uncompletedTasks" :key="task" class="p-1 w-10/12 mx-auto text-sm flex flex-row">
                         <input type="checkbox" @click="changeCompletion(task)" v-model="task.completion" class="w-5 h-5 appearance-none border-2 border-stone-200 rounded-full dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line checked:bg-pink-600"/>
                         <p class="text-sm ml-2">{{ task.content }}</p>
