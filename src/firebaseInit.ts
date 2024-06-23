44bc1c5a80e4ebcf79ae7eb1292555bfec418684
Daniel <danielfealmeida@gmail.com>
2024-06-23 16:49:49 -0300

    add database export

--- src/firebaseInit.ts
+++ src/firebaseInit.ts
@@ -1,8 +1,10 @@
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
 
 import firebaseConfig from "./firebaseConfig"
 const app = initializeApp(firebaseConfig)
 const auth = getAuth()
+const db = getFirestore()
 
-export  {app, auth}
+export  {app, auth, db}
