d82e5c751bb01b07ad8b3304d9dad072b2ff0e28
Daniel <danielfealmeida@gmail.com>
2024-06-24 20:48:08 -0300

    fix text size in auth views

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -29,7 +29,7 @@
 </script>
 
 <template>
-    <div class="login fixed top-1/2 left-1/2 border-2 border-stone-200 rounded-sm dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line flex flex-col">
+    <div class="login text-sm fixed top-1/2 left-1/2 border-2 border-stone-200 rounded-sm dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line flex flex-col">
         <h1 class="mt-10 mb-20 text-center font-bold text-lg">Entrar</h1>
         <label class="w-4/5 mx-auto">E-mail:</label>
         <input type="email" v-model="email" class="w-4/5 h-8 mx-auto mt-1 text-center border-2 border-stone-200 rounded-full dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line" @keypress.enter="sendData(email, password)"/>
--- src/views/SigninView.vue
+++ src/views/SigninView.vue
@@ -33,14 +33,14 @@
 </script>
 
 <template>
-    <div class="login fixed top-1/2 left-1/2 border-2 border-stone-200 rounded-sm dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line flex flex-col">
+    <div class="login text-sm fixed top-1/2 left-1/2 border-2 border-stone-200 rounded-sm dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line flex flex-col">
         <h1 class="mt-10 mb-20 text-center font-bold text-lg">Cadastrar</h1>
         <label class="w-4/5 mx-auto">E-mail:</label>
         <input type="email" v-model="email" class="w-4/5 h-8 mx-auto mt-1 text-center border-2 border-stone-200 rounded-full dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line" @keypress.enter="sendData(email, password)"/>
         <label class="w-4/5 mx-auto mt-10">Senha:</label>
         <input type="password" v-model="password" class="w-4/5 h-8 mx-auto mt-1 text-center border-2 border-stone-200 rounded-full dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line" @keypress.enter="sendData(email, password)"/>
         <p class="text-red-500 text-center text-sm mt-10">{{ errorCode }}</p>
-        <button @click="sendData(email, password)" class="w-2/5 h-8 mx-auto mt-120 mb-auto text-center border-2 border-stone-200 rounded-full dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line">Enviar</button>
+        <button @click="sendData(email, password)" class="w-2/5 h-8 mx-auto mt-10 mb-auto text-center border-2 border-stone-200 rounded-full dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line">Enviar</button>
         <a href="/login" class="w-2/5 h-8 mx-auto mb-auto text-center dark:bg-stone-800 dark:text-white text-sm">Já possui conta?</a>
     </div>
 </template>
