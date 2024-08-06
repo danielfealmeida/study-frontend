e1606d562e4428e5cdde841e1111e60dd62cfe73
Daniel <danielfealmeida@gmail.com>
2024-08-06 15:53:20 -0300

    add notes function

--- src/tabs/Notes.vue
+++ src/tabs/Notes.vue
@@ -1,15 +1,24 @@
 <script>
+
     export default {
-        
+        data() {
+            return {
+                notes: [{title: "Nota 1", content:"Olá"}]
+            }
+        },
     }
 </script>
 
 <template>
     <div>
-        Anotações
+        <h1 class="text-lg font-bold mb-5">Anotações</h1>
+        <div v-for="note in notes" :key="note" class="w-64 h-56 relative px-3 mr-3 mb-3 text-sm border-2 border-stone-200 rounded-sm dark:bg-stone-800 dark:text-white dark:border-b-1 dark:border-dark-line flex flex-col cursor-pointer">
+            <h1 class="mt-3 mb-1 font-semibold text-wrap truncate">{{ note.title }}</h1>
+            <p class="mt-1 text-wrap truncate">{{ note.content }}</p>
+        </div>
     </div>
 </template>
 
 <style>
-    
+
 </style>
